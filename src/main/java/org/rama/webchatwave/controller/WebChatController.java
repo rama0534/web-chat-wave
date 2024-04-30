06002f9b8938e0326ac16360c5e2e6d7e6e3194f
Rama <sadamramanayudu@gmail.com>
2024-04-30 15:06:47 -0500

    Controller added for addUser and sendMessage

---
+++ src/main/java/org/rama/webchatwave/controller/WebChatController.java
@@ -0,0 +1,24 @@
+package org.rama.webchatwave.controller;
+
+import org.rama.webchatwave.model.Chat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebChatController {
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Chat addUser(@Payload Chat message, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Chat sendMessage(@Payload Chat message) {
+        return message;
+    }
+}
