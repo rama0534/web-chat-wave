b741ef3a6a5798d27eee1e89cadd521c90b45cf6
Rama <sadamramanayudu@gmail.com>
2024-04-30 14:02:17 -0500

    Spring Initializr

---
+++ src/main/java/org/rama/webchatwave/WebChatWaveApplication.java
@@ -0,0 +1,13 @@
+package org.rama.webchatwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebChatWaveApplication.class, args);
+    }
+
+}
