e8f5a7b9dfa316097b4f87a2f194ca0226afdf5a
Rama <sadamramanayudu@gmail.com>
2024-05-01 18:09:48 -0500

    Front End

--- src/main/java/org/rama/webchatwave/controller/WebChatController.java
+++ src/main/java/org/rama/webchatwave/controller/WebChatController.java
@@ -6,8 +6,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class WebChatController {
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
